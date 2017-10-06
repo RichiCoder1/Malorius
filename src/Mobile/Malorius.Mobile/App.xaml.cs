60edf206a8ae8766c4a21b9d16920f43855efd1e
Richard Simpson <richardsimpson@outlook.com>
2017-10-06 15:32:49 -0500

    Add basic project structure

---
+++ src/Mobile/Malorius.Mobile/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Autofac;
+using Prism.Autofac.Forms;
+using Malorius.Mobile.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Malorius.Mobile
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("NavigationPage/LoginPage");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<NavigationPage>();
+            Container.RegisterTypeForNavigation<LoginPage>();
+        }
+    }
+}
